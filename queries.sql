e04974174f5b462257161d191004353bd04bd4fd
herokudev <ao43085@gmail.com>
2021-11-01 12:15:35 -0600

    Queries on animals table
    Queries on animals table

--- queries.sql
+++ queries.sql
@@ -1,4 +1,17 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-select * from vet_clinic.animals where name like '%mon'
-select * from vet_clinic.animals where date_of_birth >= '2016-01-01' and date_of_birth <= '2019-12-31'
+SELECT * FROM animals WHERE name LIKE '%mon%';
+
+SELECT * FROM animals WHERE date_of_birth BETWEEN '01-01-2016' AND '12-31-2019';
+
+SELECT name FROM animals WHERE neutered = TRUE AND escape_attempts < 3;
+
+SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
+
+SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
+
+SELECT * FROM animals WHERE neutered;
+
+SELECT * FROM animals WHERE name != 'Gabumon';
+
+SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
