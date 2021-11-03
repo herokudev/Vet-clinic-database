942443fa176bbedd26ce7cd768fba7f5ee3315a3
herokudev <ao43085@gmail.com>
2021-11-03 08:06:56 -0600

    Change status
    Change status

--- queries.sql
+++ queries.sql
@@ -63,15 +63,9 @@ SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
 
 SELECT AVG(weight_kg) FROM animals;
 
-SELECT neutered, AVG(escape_attempts)
-FROM animals
-GROUP BY neutered;
+SELECT neutered, AVG(escape_attempts) FROM animals GROUP BY neutered;
 
-SELECT species, MIN(weight_kg), MAX(weight_kg)
-FROM animals
-GROUP BY species;
+SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
 
-SELECT species, AVG(escape_attempts)
-FROM animals
-WHERE date_of_birth BETWEEN 'JAN-01-1990' AND 'DEC-31-2000'
-GROUP BY species;
+SELECT species, AVG(escape_attempts) FROM animals
+WHERE date_of_birth BETWEEN 'JAN-01-1990' AND 'DEC-31-2000' GROUP BY species;
